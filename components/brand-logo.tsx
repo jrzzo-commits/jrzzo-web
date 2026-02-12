f83563b733b6374aedbf2a98bf4725cf48079aec
jrzzo-commits <your_real_github_noreply_email>
2026-02-12 05:56:56 -0800

    Restore header logo visibility with direct SVG render

--- components/brand-logo.tsx
+++ components/brand-logo.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image';
 import Link from 'next/link';
 
 export default function BrandLogo() {
   return (
     <Link href="/" className="logo" aria-label="jrzzo home">
-      <Image src="/brand/jrzzo.svg" alt="JRZZO" width={200} height={62} className="logo-mark" priority />
+      <img src="/brand/jrzzo.svg" alt="JRZZO" className="logo-mark" />
     </Link>
   );
 }
